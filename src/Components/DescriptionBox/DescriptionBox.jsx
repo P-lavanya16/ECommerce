8fb81cc2b65d04db476989732450da4977c44934
P-lavanya16 <lavanyapuvvala16@gmail.com>
2024-05-23 10:31:36 +0530

    Submodule updates

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'> 
+    <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+    </div>
+    <div className="descriptionbox-description">
+        <p>Online sales continue to grow. By 2026, almost a quarter of all retail sales are likely to happen online—so if you have a small business, you should probably have a web store. The good news is that with the right tools, it's easy for any 
+        small business to quickly start selling online.</p>
+        <p>Online sales continue to grow. By 2026, almost a quarter of all retail sales are likely to happen online—so if you have a small business, you should probably have a web store. The good news is that with the right tools, 
+        it's easy for any small business to quickly start selling online.</p>
+    </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
